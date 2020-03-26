7f727a554b1c3ed10aea812982f650d5d81d59b1
Simon Hugot <simon.hugot.67@gmail.com>
2020-03-26 21:50:46 +0100

    Reward_heading v1

--- BSP 2/Models/code/RewardBSP.py
+++ BSP 2/Models/code/RewardBSP.py
@@ -1,3 +1,5 @@
+import math
+
 def reward_function(params):
     
     # Read input parameters
@@ -12,6 +14,7 @@ def reward_function(params):
     is_offtrack = params['is_offtrack']
     waypoints = params['waypoints']
     heading = params['heading']
+    closest_waypoints = params['closest_waypoints']
 
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
@@ -46,7 +49,31 @@ def reward_function(params):
         reward = int(progress) - int(steps)
 
     #Gives higher reward if the car is looking towards the next waypoint on the track.
+    #Create previous and incomming waypoint
+    previous = waypoints[closest_waypoints[0]]
+    incomming = waypoints[closest_waypoints[1]]
 
+    #From those waypoints coordinates calculate the the angle of the track the car needs to be headed at(-180,180 from the horizontal line).
+    #We use the atan2 and convert it to degrees
+    angle_rad = math.atan2(incomming[1] - previous[1], incomming[0] - previous[0])
+    angle_deg = math.degrees(angle_rad)
+
+    #calculate the difference between the heading of the car and the angle obtained
+    diff = abs(angle_deg - heading)
+    
+    #Maximum angle we want the car the be able to have before getting lower reward and multiple levels.
+    angle_treshold_low = 5
+    angle_treshold_mid = 10
+    angle_treshold_max = 15
+
+    if diff < angle_treshold_low:
+        reward = 10
+    elif diff < angle_treshold_mid:
+        reward = 5
+    elif diff < angle_treshold_max: 
+        reward = 1
+    elif diff > angle_treshold_max:
+        reward = 1e-3
 
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 15
@@ -66,6 +93,9 @@ def reward_function(params):
     return float(reward)
 
 """
+
+Functionnalities implemented : 
+
 - penalty if zig-zag(steering too much)
 - greater reward if closer to the middle
 - penalty if not all wheels are on track
@@ -73,6 +103,6 @@ def reward_function(params):
 - the less steps used to complete the track the better reward
 - if crash penalize
 - the more the track is completed the higher the reward 
-
 - if the car is heading towards closest waypoint better reward
+
 """
